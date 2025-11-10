750f9a4e8a3377cc975361cc518cc02c509a40f9
Mihai Gătejescu <mgatejescu@pm.me>
2025-11-10 17:23:58 +0200

    Initial commit

---
+++ src/main/java/ro/mg/chessserver/ChessserverApplication.java
@@ -0,0 +1,13 @@
+package ro.mg.chessserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ro/mg/chessserver/ChessserverApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mg.chessserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
