29e7a63de883d13715ee8838ff23c6d1b973441b
Mihai Gătejescu <mgatejescu@pm.me>
2025-12-01 20:46:00 +0200

    Change the name of the application class

--- src/main/java/ro/mg/chessserver/ChessServerApplication.java
+++ src/main/java/ro/mg/chessserver/ChessServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChessserverApplication {
+public class ChessServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChessserverApplication.class, args);
+        SpringApplication.run(ChessServerApplication.class, args);
     }
 
 }
--- src/test/java/ro/mg/chessserver/ChessServerApplicationTests.java
+++ src/test/java/ro/mg/chessserver/ChessServerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ChessserverApplicationTests {
+class ChessServerApplicationTests {
 
     @Test
     void contextLoads() {
