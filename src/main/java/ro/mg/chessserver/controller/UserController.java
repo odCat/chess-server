2d6982c5ad863fadf293f2c993ca1c2d02520e8f
Mihai Gătejescu <mgatejescu@pm.me>
2025-12-01 21:54:33 +0200

    Add the user service to the controller

--- src/main/java/ro/mg/chessserver/controller/UserController.java
+++ src/main/java/ro/mg/chessserver/controller/UserController.java
@@ -1,14 +1,24 @@
 package ro.mg.chessserver.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ro.mg.chessserver.model.Player;
+import ro.mg.chessserver.service.UserService;
+
 
 @RestController("/")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(@Autowired UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/user")
     public String register() {
