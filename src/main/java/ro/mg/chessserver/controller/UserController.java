890cfe937d2a6e795f3c21e6616d03ed39c68573
Mihai Gătejescu <mgatejescu@pm.me>
2025-12-06 15:20:39 +0200

    Add an endpoint for all users

--- src/main/java/ro/mg/chessserver/controller/UserController.java
+++ src/main/java/ro/mg/chessserver/controller/UserController.java
@@ -1,13 +1,17 @@
 package ro.mg.chessserver.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ro.mg.chessserver.model.Player;
@@ -30,6 +34,12 @@ public class UserController {
         userService.addPlayer(player);
 
         return player.toString();
+    }
+
+    @GetMapping("/all")
+    @ResponseBody
+    public List<Player> getAllPlayers() {
+        return userService.getPlayers();
     }
 
     @GetMapping
