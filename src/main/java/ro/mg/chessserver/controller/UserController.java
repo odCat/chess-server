d1d15470fe84348ec0ba79e84d7acb832dc013cd
Mihai Gătejescu <mgatejescu@pm.me>
2025-12-08 17:37:07 +0200

    Validate player registration

--- src/main/java/ro/mg/chessserver/controller/UserController.java
+++ src/main/java/ro/mg/chessserver/controller/UserController.java
@@ -1,11 +1,16 @@
 package ro.mg.chessserver.controller;
 
+import jakarta.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import ro.mg.chessserver.model.Player;
@@ -31,7 +37,7 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<Player> register(@Validated @RequestBody Player player) {
+    public ResponseEntity<Player> register(@Valid @RequestBody Player player) {
         if (userService.addPlayer(player))
             return ResponseEntity.status(HttpStatus.CREATED).body(player);
         else
@@ -63,5 +69,19 @@ public class UserController {
     public ResponseEntity<Void> delete(@RequestParam int id) {
         userService.deletePlayer(id);
         return ResponseEntity.ok().build();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName,errorMessage);
+        });
+
+        return errors;
     }
 }
--- src/main/java/ro/mg/chessserver/model/Player.java
+++ src/main/java/ro/mg/chessserver/model/Player.java
@@ -1,5 +1,9 @@
 package ro.mg.chessserver.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,9 +17,21 @@ import lombok.ToString;
 public class Player {
 
     private int id;
+
+    @NotNull(message = "Email must not be null")
+    @NotBlank(message = "Email must not be empty")
+    @Email
     private String email;
+
+    @NotNull(message = "Username must not be null")
+    @NotBlank(message = "Username must not be empty")
     private String username;
+
+    @NotNull(message = "Password must not be null")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&+=]).{8,24}$",
+            message = "Password must have 8-24 characters and include digit + lowercase + uppercase + symbol")
     private String password;
+
     private String firstName;
     private String lastName;
 
