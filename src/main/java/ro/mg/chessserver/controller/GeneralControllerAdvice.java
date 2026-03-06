4f53e27481377d35c0c18363223811070d053429
Mihai Gătejescu <mgatejescu@pm.me>
2026-03-06 11:43:37 +0200

    Return a JSON on user not found error

--- src/main/java/ro/mg/chessserver/controller/GeneralControllerAdvice.java
+++ src/main/java/ro/mg/chessserver/controller/GeneralControllerAdvice.java
@@ -2,19 +2,24 @@ package ro.mg.chessserver.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GeneralControllerAdvice {
 
     @ResponseStatus(value = HttpStatus.FORBIDDEN)
     @ExceptionHandler(exception = UsernameNotFoundException.class)
-    public void handleUsernamePasswordNotFound(HttpServletResponse response, UsernameNotFoundException exception) throws IOException {
-        response.getWriter().write(exception.getMessage());
+    public Map<String, String> handleUsernamePasswordNotFound(HttpServletResponse response, UsernameNotFoundException exception) throws IOException {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", exception.getMessage());
+        return errorResponse;
     }
 }
