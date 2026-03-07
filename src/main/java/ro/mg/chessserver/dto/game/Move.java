507bf9b8abf0190fcf052afd2c836d4e88fb2713
Mihai Gătejescu <mgatejescu@pm.me>
2026-03-07 10:58:48 +0200

    Create the move dto

---
+++ src/main/java/ro/mg/chessserver/dto/game/Move.java
@@ -0,0 +1,25 @@
+package ro.mg.chessserver.dto.game;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@ToString
+public class Move
+{
+    @NotBlank(message = "Position should not be blank")
+    private String fen;
+
+    @NotBlank(message = "From square should not be blank")
+    private String from;
+
+    @NotBlank(message = "To square should not be blank")
+    private String to;
+
+    private String promotion;
+}
