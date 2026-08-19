82637e1f347c6d4d4c1ba81f494db1c85bf735a6
Mihai Gătejescu <mgatejescu@pm.me>
2026-08-19 20:57:04 +0300

    Add PATCH to the CORS configuration

--- src/main/java/ro/mg/chessserver/config/SecurityConfig.java
+++ src/main/java/ro/mg/chessserver/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:5173"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
